3af899a2bc6b494e6c7ad9ee6cbbe2724ba5c08f
korpokings <sernaneyahya1@gmail.com>
2025-05-26 03:09:36 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Analytics />
-    <SpeedInsights/>
-  </>
-    
-);
+createRoot(document.getElementById("root")!).render(<App />);
